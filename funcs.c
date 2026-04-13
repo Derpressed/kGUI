360e4a683e7af86253b8bbea30d5ed5f9e91699e
Derpressed <61562288+Derpressed@users.noreply.github.com>
2026-04-12 19:22:41 -0500

    Add files via upload

---
+++ funcs.c
@@ -0,0 +1,97 @@
+#include <windows.h>
+#include "structs.h"
+#include <stdio.h>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    if (uMsg == WM_DESTROY) {
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProcW(hwnd, uMsg, wParam, lParam);
+}
+
+// this function is called when we create a class (kGUI)
+kGUI procK(LPCWSTR kGUIClassName) {
+    kGUI kMaster = {0};
+
+    // get our handle instance for program
+    HINSTANCE hInstance = GetModuleHandleW(NULL);
+
+    // init wc parameters (check if we need to change anything else)
+    WNDCLASSW wc = {0};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = kGUIClassName;
+    
+    // declaring our struct (this will be a passed along)
+    kMaster.valid = RegisterClassW(&wc); // returns 0 if fails
+    kMaster.hInstance = hInstance;
+    kMaster.CLASS_NAME = kGUIClassName;
+    kMaster.wc = wc;
+    
+    return kMaster;
+}
+
+// this function is called when we create a process window
+kWnd wndK(kGUI *parentClass, LPCWSTR wndName) {
+    kWnd wndMaster = {0};
+
+    wndMaster.exStyle = 0;
+    wndMaster.CLASS_NAME = parentClass->CLASS_NAME;
+    wndMaster.WINDOW_NAME = wndName;
+    wndMaster.style = WS_OVERLAPPEDWINDOW;
+    wndMaster.x = 0;
+    wndMaster.y = 0;
+    wndMaster.width = 500;
+    wndMaster.length = 200;
+    wndMaster.parent = NULL;
+    wndMaster.menu = NULL;
+    wndMaster.hInstance = parentClass->hInstance;
+    wndMaster.lpParam = NULL;
+
+    return wndMaster;
+}
+
+int showK(kWnd* kWndMain) {
+    int valid = 0;
+
+    // initializing our struct here because we can change it later
+    kWndMain->hwnd = CreateWindowExW(
+        kWndMain->exStyle,
+        kWndMain->CLASS_NAME,
+        kWndMain->WINDOW_NAME,
+        kWndMain->style,
+        kWndMain->x,
+        kWndMain->y,
+        kWndMain->width,
+        kWndMain->length,
+        kWndMain->parent,
+        kWndMain->menu,
+        kWndMain->hInstance,
+        kWndMain->lpParam
+    );
+
+
+    ShowWindow(kWndMain->hwnd, SW_SHOW);
+
+    MSG msg;
+    while (GetMessageW(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessageW(&msg);
+    }
+
+    return valid;
+}
+
+
+
+
+
+int main() {
+    
+    kGUI mainProc = procK(L"Test");
+    kWnd mainWnd = wndK(&mainProc, L"Hello World!");
+    showK(&mainWnd);
+
+    return 0;
+}
---
+++ structs.h
@@ -0,0 +1,111 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+// Code must generate a parent window first
+// struct for parent window
+typedef struct {
+    // check validity of our process
+    int valid;
+    // pointer to the handle of our program
+    HINSTANCE hInstance;
+    // class name of our main window
+    LPCWSTR CLASS_NAME;
+    // only needs lpfnWndProc, hInstance, lpszClassName
+    WNDCLASSW wc;
+    // an array for all our elements within this process
+} kGUI;
+
+// parameters for making any window function
+typedef struct {
+    DWORD     exStyle; // extended windows styles
+    LPCWSTR   CLASS_NAME; // class name of the program
+    LPCWSTR   WINDOW_NAME; // window name of this object
+    DWORD     style; // windows styles
+    int       x; // position that this window will pop up
+    int       y; // position that this window will pop up
+    int       width; // the width that this window will pop up
+    int       length; // the length that this window will pop up
+    HWND      parent; // parent handle window
+    HMENU     menu; // menu
+    HINSTANCE hInstance; // handle instance of our whole program
+    LPVOID    lpParam; // usually NULL
+
+    HWND hwnd;
+} kWnd;
+
+// interrupt function for Windows
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+// when initiallizing your process, takes a string to be called a class
+// returns your process struct
+kGUI procK(LPCWSTR kGUIClassName);
+
+// when initializing your main window, takes the process struct and window name
+// returns your window struct
+kWnd wndK(kGUI *parentClass, LPCWSTR wndName);
+
+// when running your window to be displayed, takes the window struct
+// returns an int -> 0 = valid, 1 = invalid
+int showK(kWnd* kWndMain);
+
+
+// LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+//     if (uMsg == WM_DESTROY) {
+//         PostQuitMessage(0);
+//         return 0;
+//     }
+//     return DefWindowProc(hwnd, uMsg, wParam, lParam);
+// }
+
+// int main() {
+    
+//     // give us the pointer to the handle of our own process
+//     HINSTANCE hInstance = GetModuleHandle(NULL);
+
+//     // name our class process "SimpleWindow"
+//     const char CLASS_NAME[] = "SimpleWindow";
+
+
+//     WNDCLASS wc = {0};
+//     wc.lpfnWndProc   = WindowProc;
+//     wc.hInstance     = hInstance;
+//     wc.lpszClassName = CLASS_NAME;
+
+//     RegisterClass(&wc);
+
+//     HWND hwnd = CreateWindowEx(
+//         0,
+//         CLASS_NAME,
+//         "Hello Window",
+//         WS_OVERLAPPEDWINDOW,
+//         CW_USEDEFAULT, CW_USEDEFAULT, 500, 200,
+//         NULL, NULL, hInstance, NULL
+//     );
+
+//     if (!hwnd) return 1;
+
+//     // Textbox (EDIT control)
+//     CreateWindowEx(
+//         0,
+//         "EDIT",
+//         "Hello World",
+//         WS_CHILD | WS_VISIBLE,
+//         0, 0, 100, 1,
+//         hwnd,
+//         NULL,
+//         hInstance,
+//         NULL
+//     );
+
+//     ShowWindow(hwnd, SW_SHOW);
+
+//     MSG msg;
+//     while (GetMessage(&msg, NULL, 0, 0)) {
+//         TranslateMessage(&msg);
+//         DispatchMessage(&msg);
+//     }
+
+//     return 0;
+// }
+
+#endif
